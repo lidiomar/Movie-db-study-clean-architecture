6cfaf1687fb0453571aaf2265e171be5559ab437
Lidiomar Machado <lidiomarmachado@Lidiomar-Machado-MacBook-Pro-366A.local>
2022-05-05 19:10:33 -0300

    Add API end to end test

--- MovieDB/MovieDB/API/URLSessionHTTPClient.swift
+++ MovieDB/MovieDB/API/URLSessionHTTPClient.swift
@@ -10,7 +10,7 @@ import Foundation
 public class URLSessionHTTPClient: HTTPClient {
     var session: URLSession
     
-    public init(session: URLSession = .shared) {
+    public init(session: URLSession = URLSession(configuration: URLSessionConfiguration.ephemeral)) {
         self.session = session
     }
     
---
+++ MovieDB/MovieDBAPIEndToEndTests/MovieDBAPIEndToEndTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDBAPIEndToEndTests.swift
+//  MovieDBAPIEndToEndTests
+//
+//  Created by Lidiomar Machado on 05/05/22.
+//
+
+import XCTest
+import MovieDB
+
+class MovieDBAPIEndToEndTests: XCTestCase {
+    private let urlString = "https://api.themoviedb.org/3/movie/popular?api_key=44bc59c6e912b1afda251960c4f46658&language=en-US&page=1"
+    
+    func test_retrieve20ElementsFromApiForPageNumber1() {
+        let url = URL(string: urlString)!
+        let httpClient = URLSessionHTTPClient()
+        let remoteMovieLoader = RemoteMovieLoader(url: url, httpClient: httpClient)
+        
+        let exp = expectation(description: "Wait for request")
+        remoteMovieLoader.load { result in
+            switch result {
+            case let .success(movieRoot):
+                XCTAssertEqual(movieRoot.page, 1)
+                XCTAssertEqual(movieRoot.results.count, 20)
+            default:
+                XCTFail("Expect success but \(result) was received.")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 10.0)
+    }
+}
