9c33e5fead4437cd4222aa4d09e3eb6a21af271f
Lidiomar Machado <lidiomarmachado@Lidiomar-Machado-MacBook-Pro-366A.local>
2022-05-09 18:56:43 -0300

    create insertion tests

--- MovieDB/MovieDBTests/CacheMovieUseCaseTests.swift
+++ MovieDB/MovieDBTests/CacheMovieUseCaseTests.swift
@@ -23,11 +23,13 @@ class LocalMovieLoader {
         self.timestamp = timestamp
     }
     
-    func save(movieRoot: MovieRoot) {
+    func save(movieRoot: MovieRoot, completion: @escaping (Error?) -> Void) {
         movieStore.deleteCache() { [unowned self] error in
             if error == nil {
-                self.movieStore.insert(movieRoot: movieRoot, timestamp: self.timestamp()) { _ in }
+                self.movieStore.insert(movieRoot: movieRoot, timestamp: self.timestamp(), completion: completion)
+                return
             }
+            completion(error)
         }
     }
     
@@ -45,7 +47,7 @@ class CacheMovieUseCaseTests: XCTestCase {
         let (sut, movieStore) = makeSUT()
         let movieRoot = makeMovieRoot(page: 1, movies: [makeUniqueMovie(), makeUniqueMovie()])
         
-        sut.save(movieRoot: movieRoot)
+        sut.save(movieRoot: movieRoot) { _ in }
         
         XCTAssertEqual(movieStore.receivedMessages, [.deletedCache])
     }
@@ -54,7 +56,7 @@ class CacheMovieUseCaseTests: XCTestCase {
         let (sut, movieStore) = makeSUT()
         let movieRoot = makeMovieRoot(page: 1, movies: [makeUniqueMovie(), makeUniqueMovie()])
         
-        sut.save(movieRoot: movieRoot)
+        sut.save(movieRoot: movieRoot) { _ in }
         movieStore.completeDeletion(withError: anyError())
         
         XCTAssertEqual(movieStore.receivedMessages, [.deletedCache])
@@ -65,10 +67,54 @@ class CacheMovieUseCaseTests: XCTestCase {
         let (sut, movieStore) = makeSUT(timestamp: { timestamp })
         let movieRoot = makeMovieRoot(page: 1, movies: [makeUniqueMovie(), makeUniqueMovie()])
         
-        sut.save(movieRoot: movieRoot)
+        sut.save(movieRoot: movieRoot) { _ in }
         movieStore.completeDeletionWithSuccess()
 
         XCTAssertEqual(movieStore.receivedMessages, [.deletedCache, .insert((movieRoot, timestamp))])
+    }
+    
+    func test_save_failOnDeletionError() {
+        let timestamp = Date()
+        let (sut, movieStore) = makeSUT(timestamp: { timestamp })
+        let movieRoot = makeMovieRoot(page: 1, movies: [makeUniqueMovie(), makeUniqueMovie()])
+        
+        sut.save(movieRoot: movieRoot) { _ in }
+        movieStore.completeDeletion(withError: anyError())
+        
+        XCTAssertEqual(movieStore.receivedMessages, [.deletedCache])
+    }
+    
+    func test_save_failOnInsertionError() {
+        let timestamp = Date()
+        let (sut, movieStore) = makeSUT(timestamp: { timestamp })
+        let movieRoot = makeMovieRoot(page: 1, movies: [makeUniqueMovie(), makeUniqueMovie()])
+        let error = anyError()
+        var receivedError: NSError?
+        
+        sut.save(movieRoot: movieRoot) { error in
+            receivedError = error as NSError?
+        }
+        
+        movieStore.completeDeletionWithSuccess()
+        movieStore.completeInsertionWithError(error: anyError())
+        
+        XCTAssertEqual(receivedError, error)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let timestamp = Date()
+        let (sut, movieStore) = makeSUT(timestamp: { timestamp })
+        let movieRoot = makeMovieRoot(page: 1, movies: [makeUniqueMovie(), makeUniqueMovie()])
+        var receivedError: NSError?
+        
+        sut.save(movieRoot: movieRoot) { error in
+            receivedError = error as NSError?
+        }
+        
+        movieStore.completeDeletionWithSuccess()
+        movieStore.completeInsertionWithSuccess()
+        
+        XCTAssertNil(receivedError)
     }
     
     private func makeSUT(timestamp: @escaping (() -> Date) = {  Date() }) -> (LocalMovieLoader, MovieStoreSpy) {
@@ -118,6 +164,7 @@ private class MovieStoreSpy: MovieStore {
     
     private(set) var receivedMessages = [ReceivedMessage]()
     private var deleteCacheCompletions = [(Error?) -> Void]()
+    private var insertCompletions = [(Error?) -> Void]()
     private var itemsInserted = [(MovieRoot, Date)]()
     
     func deleteCache(completion: @escaping (Error?) -> Void) {
@@ -127,6 +174,7 @@ private class MovieStoreSpy: MovieStore {
     
     func insert(movieRoot: MovieRoot, timestamp: Date, completion: @escaping (Error?) -> Void) {
         receivedMessages.append(.insert((movieRoot, timestamp)))
+        insertCompletions.append(completion)
     }
     
     func completeDeletion(withError error: NSError, at index: Int = 0) {
@@ -135,5 +183,13 @@ private class MovieStoreSpy: MovieStore {
     
     func completeDeletionWithSuccess(at index: Int = 0) {
         deleteCacheCompletions[index](nil)
+    }
+    
+    func completeInsertionWithError(at index: Int = 0, error: NSError) {
+        insertCompletions[index](error)
+    }
+    
+    func completeInsertionWithSuccess(at index: Int = 0) {
+        insertCompletions[index](nil)
     }
 }
