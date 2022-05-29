3cfe884d02f6938bd4898b8d5d44c3a2498e2e72
Lidiomar Machado <lidiomarmachado@Lidiomar-Machado-MacBook-Pro-366A.local>
2022-05-29 13:53:54 -0300

    Created MovieDBCacheIntegrationTests target

---
+++ MovieDB/MovieDBCacheIntegrationTests/MovieDBCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  MovieDBCacheIntegrationTests.swift
+//  MovieDBCacheIntegrationTests
+//
+//  Created by Lidiomar Machado on 29/05/22.
+//
+
+import XCTest
+
+class MovieDBCacheIntegrationTests: XCTestCase {
+
+}
