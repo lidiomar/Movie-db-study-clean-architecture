b557748c4ba88b2b95818373ecfdb5927f2797a7
Lidiomar Machado <lidiomarmachado@Lidiomar-Machado-MacBook-Pro-366A.local>
2022-05-29 15:21:48 -0300

    Add iOS support to run on CI

---
+++ MovieDB/MovieDBiOSTests/MovieDBiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBiOSTests.swift
+//  MovieDBiOSTests
+//
+//  Created by Lidiomar Machado on 29/05/22.
+//
+
+import XCTest
+@testable import MovieDBiOS
+
+class MovieDBiOSTests: XCTestCase {
+
+    func test_ok() {
+        XCTAssertTrue(1 == 1)
+    }
+
+}
