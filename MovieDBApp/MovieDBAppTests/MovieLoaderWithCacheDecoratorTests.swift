0a933d143d1c8cfe5de2d1ce77c5116c22b0bb3b
Lidiomar Machado <lidiomarmachado@Lidiomar-Machado-MacBook-Pro-366A.local>
2022-06-03 18:22:45 -0300

    Created MovieLoaderWithCacheDecoratorTests

---
+++ MovieDBApp/MovieDBAppTests/MovieLoaderWithCacheDecoratorTests.swift
@@ -0,0 +1,88 @@
+//
+//  MovieLoaderWithCacheSavingComposite.swift
+//  MovieDBAppTests
+//
+//  Created by Lidiomar Machado on 03/06/22.
+//
+
+import Foundation
+import XCTest
+import MovieDB
+
+protocol MovieCache {}
+
+class MovieLoaderWithCacheDecorator: MovieLoader {
+    
+    private var decoratee: MovieLoader
+    private var cache: MovieCache
+    
+    init(decoratee: MovieLoader, cache: MovieCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    func load(completion: @escaping (MovieLoaderResult) -> Void) {
+        decoratee.load(completion: completion)
+    }
+}
+
+class MovieLoaderWithCacheDecoratorTests: XCTestCase {
+    
+    func test_load_deliversSuccessOnDecorateeSuccess() {
+        let movieRoot = MovieRoot(page: 1, results: [makeUniqueMovie()])
+        let sut = makeSUT(result: .success(movieRoot))
+        
+        expect(sut: sut, with: .success(movieRoot))
+    }
+    
+    private func expect(sut: MovieLoader, with expectedResult: MovieLoader.MovieLoaderResult) {
+        let exp = expectation(description: "Wait for load")
+        
+        sut.load { result in
+            switch (result, expectedResult) {
+            case let (.success(receivedMovieRoot), .success(expectedMovieRoot)):
+                XCTAssertEqual(receivedMovieRoot, expectedMovieRoot)
+            default:
+                XCTFail("Expected success, got \(result) instead.")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func makeSUT(result: MovieLoader.MovieLoaderResult) -> MovieLoader {
+        let loader = MovieLoaderStub(result: result)
+        let cache = MovieCacheSpy()
+        let sut = MovieLoaderWithCacheDecorator(decoratee: loader, cache: cache)
+        return sut
+    }
+    
+    
+    private func makeUniqueMovie() -> Movie {
+        return Movie(posterPath: nil,
+                     overview: "An overview",
+                     releaseDate: "2018-09-09",
+                     genreIds: [1, 2],
+                     id: UUID().hashValue,
+                     title: "a title",
+                     popularity: 0.0,
+                     voteCount: 0,
+                     voteAverage: 0.0)
+    }
+    
+    private class MovieLoaderStub: MovieLoader {
+        var result: MovieLoaderResult
+        
+        init(result: MovieLoaderResult) {
+            self.result = result
+        }
+        
+        func load(completion: @escaping (MovieLoaderResult) -> Void) {
+            completion(result)
+        }
+    }
+    
+    private class MovieCacheSpy: MovieCache {
+        
+    }
+}
